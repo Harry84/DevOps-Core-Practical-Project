345e24516f444fbffb541aa05902c81259de6bb2
harry <harry.leventon@gmail.com>
2022-05-03 15:46:16 +0000

    test not working yet

---
+++ pilot_api/test/test_unit_2.py
@@ -0,0 +1,21 @@
+from application import app
+from application import pilot_name
+from flask import url_for
+from flask_testing import TestCase
+
+# from application import app
+# from application import pilot_name
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_get_pilot(self):
+
+        for i in range(10):
+            response = self.client.get(url_for('get_pilot'))
+
+            self.assert200(response)
+            self.assertIn(response.data.decode(), pilot_name)
