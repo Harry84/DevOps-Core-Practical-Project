ee2728f536cd4f489efe7d1b419f865f8e391408
harry <harry.leventon@gmail.com>
2022-05-04 14:14:06 +0000

    added a test for services 2 and 3

--- tier_api/app.py
+++ tier_api/app.py
@@ -1,5 +1,13 @@
-from flask import Flask 
-from application import app 
+from flask import Flask, request, Response 
+import random
+
+app = Flask(__name__)
+
+tier_choice = ["S Tier", "A Tier", "B Tier", "C Tier", "Just Bad", "Uninstall Now"]
+
+@app.route('/get_tier',methods=['GET'])
+def name():  
+    return random.choice(tier_choice)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
