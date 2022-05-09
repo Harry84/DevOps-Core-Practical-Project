5db6776b3d6968ac559d2821cd8abc99309ea6ca
harry <harry.leventon@gmail.com>
2022-05-09 16:43:03 +0000

    minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@
 # EOF
 
 '''
-sh "scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml swarm-master:/home/jenkins/docker-compose.yaml"
-sh "scp -i ~/.ssh/ansible_id_rsa nginx.conf swarm-master:/home/jenkins/nginx.conf"
+sh "scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml swarm-manager:/home/jenkins/docker-compose.yaml"
+sh "scp -i ~/.ssh/ansible_id_rsa nginx.conf swarm-manager:/home/jenkins/nginx.conf"
 sh "ansible-playbook -i ansible/inventory.yaml ansible/playbook.yaml"
 '''
 
